451c998ca9ed429841779b442cf9651dabf4de27
AzureViolin <Hao@Dorabot.com>
2014-11-09 19:38:07 +0800

    added possible tech choice in mindmap

--- Picking Challenge.mm
+++ Picking Challenge.mm
@@ -5,6 +5,10 @@
 <node CREATED="1415387541593" ID="ID_1690798442" MODIFIED="1415387570246" TEXT="INPUT: raw vision data"/>
 <node CREATED="1415387572360" ID="ID_744228911" MODIFIED="1415387621627" TEXT="OUTPUT: Name, Pose, Position of all target object in the scene"/>
 <node CREATED="1415387625093" ID="ID_1960787901" MODIFIED="1415387838792" TEXT="All object are known to the system in any possible form of choice, including picture template, RGB-D Pointcloud, mesh, feature description of certain part and so on"/>
+<node CREATED="1415532077412" ID="ID_1645786842" MODIFIED="1415532279577" TEXT="Possible Tech">
+<node CREATED="1415532133078" ID="ID_424307237" MODIFIED="1415532137927" TEXT="LineMod"/>
+<node CREATED="1415532204692" ID="ID_1467393251" MODIFIED="1415532211843" TEXT="PCL"/>
+</node>
 </node>
 <node CREATED="1415387281915" ID="ID_1046341303" MODIFIED="1415387286917" POSITION="right" TEXT="Grasp Planning">
 <node CREATED="1415387743439" ID="ID_1583582966" MODIFIED="1415387788816" TEXT="INPUT: Object Name, Position, Pose, and other possible attributes"/>
@@ -13,6 +17,7 @@
 <node CREATED="1415388351933" ID="ID_1103997626" MODIFIED="1415388553582" TEXT="Grasping pose of end effector"/>
 </node>
 <node CREATED="1415388425759" ID="ID_217123538" MODIFIED="1415388482227" TEXT="Since all object are pre-known, so a major portion of grasp strategies can be generated offline to save time"/>
+<node CREATED="1415532256631" ID="ID_492584285" MODIFIED="1415532271060" TEXT="Possible Tech"/>
 </node>
 <node CREATED="1415387287299" ID="ID_1232811116" MODIFIED="1415495532698" POSITION="right" TEXT="Environmental Awareness">
 <node CREATED="1415495630234" ID="ID_109057377" MODIFIED="1415495792831" TEXT="1.Scan the work environment, match shelf model to the scanned data"/>
@@ -22,10 +27,16 @@
 </node>
 <node CREATED="1415496060029" ID="ID_897206453" MODIFIED="1415496114275" TEXT="3. Either use RGBD Global or RGBD End to monitor/scan the environment during each step of motion execution"/>
 <node CREATED="1415496237638" ID="ID_1602970587" MODIFIED="1415496314461" TEXT="4. Might need self-filter for the global camera to filter the robot arm"/>
+<node CREATED="1415532321881" ID="ID_1323835655" MODIFIED="1415532330923" TEXT="Possible Tech">
+<node CREATED="1415532331909" ID="ID_799438464" MODIFIED="1415532334554" TEXT="Octomap"/>
+</node>
 </node>
 <node CREATED="1415387302135" ID="ID_1780046207" MODIFIED="1415387325398" POSITION="right" TEXT="Path &amp; Motion Planning">
 <node CREATED="1415496119782" ID="ID_1756610840" MODIFIED="1415496234754" TEXT="INPUT: Grasping pose of end effector, Current robot pose, environment model with obstacle updated constantly"/>
 <node CREATED="1415496362409" ID="ID_1055279376" MODIFIED="1415496411725" TEXT="OUTPUT: Time-saving trajectory with good dynamics performance"/>
+<node CREATED="1415532338656" ID="ID_1326637690" MODIFIED="1415532342591" TEXT="Possible Tech">
+<node CREATED="1415532343401" ID="ID_310144083" MODIFIED="1415532402701" TEXT="OMPL"/>
+</node>
 </node>
 <node CREATED="1415387344163" ID="ID_232375847" MODIFIED="1415387472582" POSITION="right" TEXT="Compliant Manipulation/ Motion Execution">
 <node CREATED="1415496422105" ID="ID_1572898119" MODIFIED="1415496435336" TEXT="Baxter">
